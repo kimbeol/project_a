3e2dd1d49d9e3ab41f3187036b25f40c5e50a8a6
kimbeol <kmpluto83@gmail.com>
2022-09-22 18:33:51 +0900

    5 commit

--- js/common.js
+++ js/common.js
@@ -1,5 +1,11 @@
 
-// 1.
+/**
+ * 0.valiable
+ */
+
+/**
+ * 1.read
+ */
 read = () => {
 
     let itemsEle = document.querySelector('.items');
@@ -15,14 +21,96 @@ read = () => {
     
     eleHtml.innerHTML = elementsVal;
     
-    // html
+    // html dot elements
     itemsEle.append(eleHtml);
 
+    return  arrEvent();
 }
 
+/**
+ * 2. array event
+ */
+function arrEvent() {
+    let arrLoop = [
+        // shirt
+        {
+            "type" : "shirt",
+            "gender" : "famale",
+            "size" : "large",
+            "color" : "blue",
+            "image" : "img/blue_t.png"
+        },
+        {
+            "type" : "shirt",
+            "gender" : "famale",
+            "size" : "yellow",
+            "color" : "pink",
+            "image" : "img/yellow_t.png"
+        },
+        {
+            "type" : "shirt",
+            "gender" : "famale",
+            "size" : "large",
+            "color" : "pink",
+            "image" : "img/pink_t.png"
+        },
+        // pants
+        {
+            "type" : "pants",
+            "gender" : "famale",
+            "size" : "large",
+            "color" : "pink",
+            "image" : "img/pink_t.png"
+        },
+        {
+            "type" : "pants",
+            "gender" : "famale",
+            "size" : "large",
+            "color" : "pink",
+            "image" : "img/pink_t.png"
+        },
+        {
+            "type" : "pants",
+            "gender" : "famale",
+            "size" : "large",
+            "color" : "pink",
+            "image" : "img/pink_t.png"
+        },
+        // skirt
+        {
+            "type" : "skirt",
+            "gender" : "famale",
+            "size" : "large",
+            "color" : "pink",
+            "image" : "img/pink_t.png"
+        },
+        {
+            "type" : "skirt",
+            "gender" : "famale",
+            "size" : "large",
+            "color" : "pink",
+            "image" : "img/pink_t.png"
+        },
+        {
+            "type" : "skirt",
+            "gender" : "famale",
+            "size" : "large",
+            "color" : "pink",
+            "image" : "img/pink_t.png"
+        },
+    ];
+
+    for(val in arrLoop) {
+        console.log(arrLoop[val].type)
+    }
+}
+
+
+/**
+ * load
+ */
 window.onload = function() {
     
     // 목록
     read();
-
 }
