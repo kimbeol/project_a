bdaeb9a6c7d1c076c438124c9855ec8f79d8a60e
kimbeol <kmpluto83@gmail.com>
2022-09-26 18:16:16 +0900

    지역 scope 수정

--- js/common.js
+++ js/common.js
@@ -1,168 +1,175 @@
-/**
- * 1.load
- */
- window.onload = function() {
-    let eleVal;
-
-    // 목록
-    read();
-}
 
 
-/**
- * 0. array list
- * type : shirt, pants, skirt
- * color : blue, yellow, pink
- */
- function arrEvent() {
-    let arrLoop = [
-        // shirt
-        {
-            "type"   : "shirt",
-            "gender" : "famale",
-            "size"   : "large",
-            "color"  : "blue",
-            "image"  : "img/blue_t.png"
-        },
-        {
-            "type"   : "shirt",
-            "gender" : "male",
-            "size"   : "large",
-            "color"  : "yellow",
-            "image"  : "img/yellow_t.png"
-        },
-        {
-            "type"   : "shirt",
-            "gender" : "male",
-            "size"   : "large",
-            "color"  : "pink",
-            "image"  : "img/pink_t.png"
-        },
-        // pants
-        {
-            "type"   : "pants",
-            "gender" : "male",
-            "size"   : "small",
-            "color"  : "blue",
-            "image"  : "img/blue_p.png"
-        },
-        {
-            "type"   : "pants",
-            "gender" : "male",
-            "size"   : "large",
-            "color"  : "yellow",
-            "image"  : "img/yellow_p.png"
-        },
-        {
-            "type"   : "pants",
-            "gender" : "famale",
-            "size"   : "large",
-            "color"  : "pink",
-            "image"  : "img/pink_p.png"
-        },
-        // skirt
-        {
-            "type"   : "skirt",
-            "gender" : "famale",
-            "size"   : "small",
-            "color"  : "blue",
-            "image"  : "img/blue_s.png"
-        },
-        {
-            "type"   : "skirt",
-            "gender" : "famale",
-            "size"   : "large",
-            "color"  : "yellow",
-            "image"  : "img/yellow_s.png"
-        },
-        {
-            "type"   : "skirt",
-            "gender" : "famale",
-            "size"   : "large",
-            "color"  : "pink",
-            "image"  : "img/pink_s.png"
+window.onlineShop_list = (function() {
+
+    /**
+     * 1.load
+     */
+    window.onload = function() {
+        let eleVal;
+
+        // 목록
+        read();
+    }
+
+    /**
+     * 0. array list
+     * type : shirt, pants, skirt
+     * color : blue, yellow, pink
+     */
+    function arrEvent() {
+        let arrLoop = [
+            // shirt
+            {
+                "type"   : "shirt",
+                "gender" : "famale",
+                "size"   : "large",
+                "color"  : "blue",
+                "image"  : "img/blue_t.png"
+            },
+            {
+                "type"   : "shirt",
+                "gender" : "male",
+                "size"   : "large",
+                "color"  : "yellow",
+                "image"  : "img/yellow_t.png"
+            },
+            {
+                "type"   : "shirt",
+                "gender" : "male",
+                "size"   : "large",
+                "color"  : "pink",
+                "image"  : "img/pink_t.png"
+            },
+            // pants
+            {
+                "type"   : "pants",
+                "gender" : "male",
+                "size"   : "small",
+                "color"  : "blue",
+                "image"  : "img/blue_p.png"
+            },
+            {
+                "type"   : "pants",
+                "gender" : "male",
+                "size"   : "large",
+                "color"  : "yellow",
+                "image"  : "img/yellow_p.png"
+            },
+            {
+                "type"   : "pants",
+                "gender" : "famale",
+                "size"   : "large",
+                "color"  : "pink",
+                "image"  : "img/pink_p.png"
+            },
+            // skirt
+            {
+                "type"   : "skirt",
+                "gender" : "famale",
+                "size"   : "small",
+                "color"  : "blue",
+                "image"  : "img/blue_s.png"
+            },
+            {
+                "type"   : "skirt",
+                "gender" : "famale",
+                "size"   : "large",
+                "color"  : "yellow",
+                "image"  : "img/yellow_s.png"
+            },
+            {
+                "type"   : "skirt",
+                "gender" : "famale",
+                "size"   : "large",
+                "color"  : "pink",
+                "image"  : "img/pink_s.png"
+            }
+        ];
+
+        return arrLoop;
+    }
+
+    /**
+     * 2.read
+     */
+    read = (item) => {
+        let elementsVal2 = listHtml(item);
+
+        // html elements
+        let itemsEle = document.querySelector('.items');
+        itemsEle.innerHTML = elementsVal2;
+
+        // return arrEvent();
+    }
+
+    /**
+     * search Event
+     */
+    searchList = (item) => {
+        let dataName = item.dataset.action;
+        let listLi = [];
+        let lengthLi = arrEvent();
+        let schEle = '';
+
+        for(val of lengthLi) {
+            // console.log(val.image);
+            if((dataName == val.type) || (dataName == val.color)) {
+                schEle += `<li class="li" data-code="${val.type}" data-color="${val.color}">
+                        <img src="${val.image}" alt="${val.type}">
+                        <span class="name">${val.gender}, ${val.size}</span>
+                    </li>`;
+            }
         }
-    ];
 
-    return arrLoop;
-}
+        let itemsEle = document.querySelector('.items');
+        delete itemsEle.children;
+        itemsEle.innerHTML = schEle;
+        
+    }
 
-/**
- * 2.read
- */
-read = (item) => {
-    let elementsVal2 = listHtml(item);
+    /**
+     * function
+     */
+    listHtml = (item) => {
+        let items = item;
+        let arrEventList = arrEvent();
+        let elementsVal = '';
+        let elementsValArr = [];
 
-    // html elements
-    let itemsEle = document.querySelector('.items');
-    itemsEle.innerHTML = elementsVal2;
+        for(val of arrEventList) {
 
-    // return arrEvent();
-}
-
-/**
- * search Event
- */
-searchList = (item) => {
-    let dataName = item.dataset.action;
-    let listLi = [];
-    let lengthLi = arrEvent();
-    let schEle = '';
-
-    for(val of lengthLi) {
-        // console.log(val.image);
-        if((dataName == val.type) || (dataName == val.color)) {
-            schEle += `<li class="li" data-code="${val.type}" data-color="${val.color}">
+            elementsVal += `<li class="li" data-code="${val.type}" data-color="${val.color}">
                     <img src="${val.image}" alt="${val.type}">
                     <span class="name">${val.gender}, ${val.size}</span>
                 </li>`;
         }
+
+        let ele = elementsVal;
+        return ele;
     }
 
-    let itemsEle = document.querySelector('.items');
-    delete itemsEle.children;
-    itemsEle.innerHTML = schEle;
+    // 이벤트 위임
+    // const ActionFunctions = {
+    //     shirt : () => {searchList('shirt')},
+    //     pants : () => {searchList('pants')},
+    //     skirt : () => {searchList('skirt')},
+    //     blue :  () => {searchList('blue')},
+    //     yellow : () => {searchList('yellow')},
+    //     pink :  () => {searchList('pink')}
+    // }
+
+    /**
+     * click button event
+     */
+    document.querySelector('.buttons').addEventListener('click', e =>{
     
-}
+        const action = e.target;
+        searchList(action);
 
-/**
- * function
- */
-listHtml = (item) => {
-    let items = item;
-    let arrEventList = arrEvent();
-    let elementsVal = '';
-    let elementsValArr = [];
+    }); 
 
-    for(val of arrEventList) {
+})();
 
-        elementsVal += `<li class="li" data-code="${val.type}" data-color="${val.color}">
-                <img src="${val.image}" alt="${val.type}">
-                <span class="name">${val.gender}, ${val.size}</span>
-            </li>`;
-    }
 
-    let ele = elementsVal;
-    return ele;
-}
-
-// 이벤트 위임
-// const ActionFunctions = {
-//     shirt : () => {searchList('shirt')},
-//     pants : () => {searchList('pants')},
-//     skirt : () => {searchList('skirt')},
-//     blue :  () => {searchList('blue')},
-//     yellow : () => {searchList('yellow')},
-//     pink :  () => {searchList('pink')}
-// }
-
-/**
- * click button event
- */
-document.querySelector('.buttons').addEventListener('click', e =>{
-  
-    const action = e.target;
-    searchList(action);
-
-});
 
